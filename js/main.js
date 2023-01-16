730677deed210906dd4c2d107e23cde06e3e3edc
Yakyzzza163 <ksport124@gmail.com>
2023-01-16 20:26:02 +0700

    Add

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+var area = document.getElementById('area');
+var cell = document.getElementsByClassName ('cell');
+
+var player = "x";
+var winIndex = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9],
+	[2,5,8],
+	[3,6,9],
+	[1,5,9],
+	[3,5,7],
+	[1,4,7],
+];
+
+for(var i = 1; i <= 9; i++) {
+	area.innerHTML += "<div class='cell' pos=" + i + "></div";
+}
+
+for (var i = 0; i<cell.length; i++) {
+	cell[i].addEventListener('click', cellclick, false);
+}
+
+function cellclick () {
+
+	var data = [];
+
+	if(!this.innerHTML) {
+		this.innerHTML = player;
+	}else {
+		alert("Занято");
+		return;
+	}
+
+	for(var i in cell){
+		if(cell[i].innerHTML == player){
+			data.push(parseInt(cell[i].getAttribute('pos')));
+		}
+	}
+
+
+	if(checkWin(data)) {
+		restart("Победа! " + player);
+	}else {
+		var draw = true;
+		for(var i in cell){
+			if(cell[i].innerHTML == '') draw = false;
+		}
+		if(draw) {
+			restart(Ничья); 
+		}
+	}
+
+	player = player == "x" ? "o" : "x";
+}
+
+function checkWin(data) {
+	for(var i in winIndex) {
+		var win = true;
+		for(var j in winIndex[i]) {
+			 var id = winIndex[i][j];
+			 var ind = data.indexOf(id);
+
+			 if(ind == -1) {
+				win = false
+			 }
+		}
+		
+		if(win) return true;
+	}
+	return false;
+	
+}
+
+function restart (text) {
+	alert(text);
+	for(var i = 0; i < cell.length; i++) {
+		cell[i].innerHTML = '';
+	}
+
+}
